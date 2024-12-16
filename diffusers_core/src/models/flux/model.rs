b3eea6f4feb2180d4f8773b785f4a3e1af4b83b4
Eric Buehler <65165915+EricLBuehler@users.noreply.github.com>
2024-12-16 19:46:51 +0100

    Run flux sdpa in f32 (#3)

--- diffusers_core/src/models/flux/model.rs
+++ diffusers_core/src/models/flux/model.rs
@@ -36,8 +36,27 @@ fn layer_norm(dim: usize, vb: VarBuilder) -> Result<LayerNorm> {
 
 fn scaled_dot_product_attention(q: &Tensor, k: &Tensor, v: &Tensor) -> Result<Tensor> {
     let dim = q.dim(D::Minus1)?;
-    let scale_factor = 1.0 / (dim as f32).sqrt();
-    diffusers_backend::ops::sdpa(q, k, v, scale_factor, 1.0)
+    let scale_factor = 1.0 / (dim as f64).sqrt();
+    diffusers_backend::ops::sdpa(
+        &q.to_dtype(DType::F32)?,
+        &k.to_dtype(DType::F32)?,
+        &v.to_dtype(DType::F32)?,
+        scale_factor as f32,
+        1.0,
+    )?
+    .to_dtype(q.dtype())
+
+    // let mut batch_dims = q.dims().to_vec();
+    // batch_dims.pop();
+    // batch_dims.pop();
+    // let q = q.flatten_to(batch_dims.len() - 1)?;
+    // let k = k.flatten_to(batch_dims.len() - 1)?;
+    // let v = v.flatten_to(batch_dims.len() - 1)?;
+    // let attn_weights = (q.matmul(&k.t()?)? * scale_factor)?;
+    // let attn_scores = candle_nn::ops::softmax_last_dim(&attn_weights)?.matmul(&v)?;
+    // batch_dims.push(attn_scores.dim(D::Minus2)?);
+    // batch_dims.push(attn_scores.dim(D::Minus1)?);
+    // attn_scores.reshape(batch_dims)
 }
 
 fn rope(pos: &Tensor, dim: usize, theta: usize) -> Result<Tensor> {
