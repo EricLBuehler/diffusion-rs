5c3b501a455ffc228d4392871d242a411b0c3aad
Eric Buehler <65165915+EricLBuehler@users.noreply.github.com>
2024-12-22 23:16:03 +0100

    Add python api type hint file (#14)
    * Add python api type hint file
    
    * Add python api type hint file

--- diffuse_rs_common/src/core/tests/pth.py
+++ diffuse_rs_common/src/core/tests/pth.py
@@ -2,7 +2,7 @@ import torch
 from collections import OrderedDict
 
 # Write a trivial tensor to a pt file
-a= torch.tensor([[1,2,3,4], [5,6,7,8]])
+a = torch.tensor([[1, 2, 3, 4], [5, 6, 7, 8]])
 o = OrderedDict()
 o["test"] = a
 
@@ -19,19 +19,25 @@ import numpy as np
 
 # Step 1: Create a 3D NumPy array with Fortran order using a range of numbers
 # For example, creating a 2x3x4 array
-array_fortran = np.asfortranarray(np.arange(1, 2*3*4 + 1).reshape(2, 3, 4))
+array_fortran = np.asfortranarray(np.arange(1, 2 * 3 * 4 + 1).reshape(2, 3, 4))
 
 # Verify the memory order
-print("Is Fortran contiguous (F order):", array_fortran.flags['F_CONTIGUOUS'])  # Should be True
-print("Is C contiguous (C order):", array_fortran.flags['C_CONTIGUOUS'])  # Should be False
+print(
+    "Is Fortran contiguous (F order):", array_fortran.flags["F_CONTIGUOUS"]
+)  # Should be True
+print(
+    "Is C contiguous (C order):", array_fortran.flags["C_CONTIGUOUS"]
+)  # Should be False
 
 # Step 2: Convert the NumPy array to a PyTorch tensor
 tensor_fortran = torch.from_numpy(array_fortran)
 
 # Verify the tensor layout
-print("Tensor stride:", tensor_fortran.stride())  # Stride will reflect the Fortran memory layout
+print(
+    "Tensor stride:", tensor_fortran.stride()
+)  # Stride will reflect the Fortran memory layout
 
 # Step 3: Save the PyTorch tensor to a .pth file
-torch.save({"tensor_fortran": tensor_fortran}, 'fortran_tensor_3d.pth')
+torch.save({"tensor_fortran": tensor_fortran}, "fortran_tensor_3d.pth")
 
 print("3D Tensor saved with Fortran layout.")
