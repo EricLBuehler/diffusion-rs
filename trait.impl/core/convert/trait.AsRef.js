72d8b9cdc7da0a4661b541c7e2f6b942e5149333
EricLBuehler <65165915+EricLBuehler@users.noreply.github.com>
2025-04-01 02:08:33 +0000

    Deploying to gh-pages from @ EricLBuehler/diffusion-rs@d977e05ad19075f87bed8c0888ae42e63c456729 🚀

--- trait.impl/core/convert/trait.AsRef.js
+++ trait.impl/core/convert/trait.AsRef.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["diffusion_rs_common",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.83.0/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;<a class=\"struct\" href=\"diffusion_rs_common/core/struct.Tensor.html\" title=\"struct diffusion_rs_common::core::Tensor\">Tensor</a>&gt; for <a class=\"struct\" href=\"diffusion_rs_common/core/struct.Tensor.html\" title=\"struct diffusion_rs_common::core::Tensor\">Tensor</a>"]]]]);
+    var implementors = Object.fromEntries([["diffusion_rs_common",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.1/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;<a class=\"struct\" href=\"diffusion_rs_common/core/struct.Tensor.html\" title=\"struct diffusion_rs_common::core::Tensor\">Tensor</a>&gt; for <a class=\"struct\" href=\"diffusion_rs_common/core/struct.Tensor.html\" title=\"struct diffusion_rs_common::core::Tensor\">Tensor</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
--- trait.impl/core/iter/traits/iterator/trait.Iterator.js
+++ trait.impl/core/iter/traits/iterator/trait.Iterator.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["diffusion_rs_common",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.83.0/core/iter/traits/iterator/trait.Iterator.html\" title=\"trait core::iter::traits::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"diffusion_rs_common/core/struct.StridedIndex.html\" title=\"struct diffusion_rs_common::core::StridedIndex\">StridedIndex</a>&lt;'_&gt;"]]]]);
+    var implementors = Object.fromEntries([["diffusion_rs_common",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.1/core/iter/traits/iterator/trait.Iterator.html\" title=\"trait core::iter::traits::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"diffusion_rs_common/core/struct.StridedIndex.html\" title=\"struct diffusion_rs_common::core::StridedIndex\">StridedIndex</a>&lt;'_&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
